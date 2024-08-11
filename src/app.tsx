57d7ed600d46f32a5afc6be2458d0e8b43dc5ed2
professionalowo <martin.brandl1307@gmail.com>
2024-08-11 21:27:58 +0200

    moved in router

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,16 @@
 import { CompressForm } from "./components/CompressForm";
+import { Router } from "./components/router/Router";
 
 export function App() {
-  return <CompressForm />;
+  return <Root />;
 }
+
+const Root = () => (
+  <Router
+    routes={[
+      Router.Route({ path: "/", element: <CompressForm /> }),
+      Router.Route({ path: "/about", element: <p>About</p> }),
+    ]}
+    notFound={<p>Not found</p>}
+  />
+);
